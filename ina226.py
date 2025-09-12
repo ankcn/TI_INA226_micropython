c85b4c14445c152f41f1c48d7adceaf6dfc3b6b9
Andrey Tarasov <kochenevo@gmail.com>
2025-09-12 19:16:16 +0700

    Make configuration constants public
    Removed underscore from configuration constants for allowing them externally accessible.
    Fix some typo's.
    Removed carriage returns.

--- ina226.py
+++ ina226.py
@@ -39,74 +39,79 @@ from micropython import const
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/elschopi/TI_INA226_micropython.git"
 
-# Bits
-# pylint: disable=bad-whitespace
-_READ = const(0x01)
+
+# Register definitions
 
 # Config Register (R/W)
 _REG_CONFIG = const(0x00)
-_CONFIG_RESET = const(0x8000)  # Reset Bit
-# Constant bits - don't change
-_CONFIG_CONST_BITS = const(0x4000)
-# Averaging mode
-_CONFIG_AVGMODE_MASK = const(0x0e00)
-_CONFIG_AVGMODE_1SAMPLES = const(0x0000)
-_CONFIG_AVGMODE_4SAMPLES = const(0x0200)
-_CONFIG_AVGMODE_16SAMPLES = const(0x0400)
-_CONFIG_AVGMODE_64SAMPLES = const(0x0600)
-_CONFIG_AVGMODE_128SAMPLES = const(0x0800)
-_CONFIG_AVGMODE_256SAMPLES = const(0x0a00)
-_CONFIG_AVGMODE_512SAMPLES = const(0x0c00)
-_CONFIG_AVGMODE_1024SAMPLES = const(0x0e00)
 
-# Bus voltage conversion time
-_CONFIG_VBUSCT_MASK = const(0x01c0)
-_CONFIG_VBUSCT_140us = const(0x0000)
-_CONFIG_VBUSCT_204us = const(0x0040)
-_CONFIG_VBUSCT_332us = const(0x0080)
-_CONFIG_VBUSCT_588us = const(0x00c0)
-_CONFIG_VBUSCT_1100us = const(0x0100)
-_CONFIG_VBUSCT_21116us = const(0x0140)
-_CONFIG_VBUSCT_4156us = const(0x0180)
-_CONFIG_AVGMODE_8244us = const(0x01c0)
-
-# Shunt voltage conversion time
-_CONFIG_VSHUNTCT_MASK = const(0x0038)
-_CONFIG_VSHUNTCT_140us = const(0x0000)
-_CONFIG_VSHUNTCT_204us = const(0x0008)
-_CONFIG_VSHUNTCT_332us = const(0x0010)
-_CONFIG_VSHUNTCT_588us = const(0x0018)
-_CONFIG_VSHUNTCT_1100us = const(0x0020)
-_CONFIG_VSHUNTCT_21116us = const(0x0028)
-_CONFIG_VSHUNTCT_4156us = const(0x0030)
-_CONFIG_VSHUNTCT_8244us = const(0x0038)
-
-# Operating mode
-_CONFIG_MODE_MASK = const(0x0007)  # Operating Mode Mask
-_CONFIG_MODE_POWERDOWN = const(0x0000)
-_CONFIG_MODE_SVOLT_TRIGGERED = const(0x0001)
-_CONFIG_MODE_BVOLT_TRIGGERED = const(0x0002)
-_CONFIG_MODE_SANDBVOLT_TRIGGERED = const(0x0003)
-_CONFIG_MODE_ADCOFF = const(0x0004)
-_CONFIG_MODE_SVOLT_CONTINUOUS = const(0x0005)
-_CONFIG_MODE_BVOLT_CONTINUOUS = const(0x0006)
-_CONFIG_MODE_SANDBVOLT_CONTINUOUS = const(0x0007)
-
-# SHUNT VOLTAGE REGISTER (R)
+# Shunt voltage register (R)
 _REG_SHUNTVOLTAGE = const(0x01)
 
-# BUS VOLTAGE REGISTER (R)
+# Bus voltage register (R)
 _REG_BUSVOLTAGE = const(0x02)
 
-# POWER REGISTER (R)
+# Power register (R)
 _REG_POWER = const(0x03)
 
-# CURRENT REGISTER (R)
+# Current register (R)
 _REG_CURRENT = const(0x04)
 
-# CALIBRATION REGISTER (R/W)
+# Calibration register (R/W)
 _REG_CALIBRATION = const(0x05)
-# pylint: enable=bad-whitespace
+
+
+# Configuration register values
+
+# Reset Bit
+CONFIG_RESET = const(0x8000)
+
+# Constant bits - don't change
+CONFIG_CONST_BITS = const(0x4000)
+
+# Averaging mode
+CONFIG_AVGMODE_MASK = const(0x0e00)
+CONFIG_AVGMODE_1SAMPLES = const(0x0000)
+CONFIG_AVGMODE_4SAMPLES = const(0x0200)
+CONFIG_AVGMODE_16SAMPLES = const(0x0400)
+CONFIG_AVGMODE_64SAMPLES = const(0x0600)
+CONFIG_AVGMODE_128SAMPLES = const(0x0800)
+CONFIG_AVGMODE_256SAMPLES = const(0x0a00)
+CONFIG_AVGMODE_512SAMPLES = const(0x0c00)
+CONFIG_AVGMODE_1024SAMPLES = const(0x0e00)
+
+# Bus voltage conversion time
+CONFIG_VBUSCT_MASK = const(0x01c0)
+CONFIG_VBUSCT_140us = const(0x0000)
+CONFIG_VBUSCT_204us = const(0x0040)
+CONFIG_VBUSCT_332us = const(0x0080)
+CONFIG_VBUSCT_588us = const(0x00c0)
+CONFIG_VBUSCT_1100us = const(0x0100)
+CONFIG_VBUSCT_2116us = const(0x0140)
+CONFIG_VBUSCT_4156us = const(0x0180)
+CONFIG_VBUSCT_8244us = const(0x01c0)
+
+# Shunt voltage conversion time
+CONFIG_VSHUNTCT_MASK = const(0x0038)
+CONFIG_VSHUNTCT_140us = const(0x0000)
+CONFIG_VSHUNTCT_204us = const(0x0008)
+CONFIG_VSHUNTCT_332us = const(0x0010)
+CONFIG_VSHUNTCT_588us = const(0x0018)
+CONFIG_VSHUNTCT_1100us = const(0x0020)
+CONFIG_VSHUNTCT_2116us = const(0x0028)
+CONFIG_VSHUNTCT_4156us = const(0x0030)
+CONFIG_VSHUNTCT_8244us = const(0x0038)
+
+# Operating mode
+CONFIG_MODE_MASK = const(0x0007)  # Operating Mode Mask
+CONFIG_MODE_POWERDOWN = const(0x0000)
+CONFIG_MODE_SVOLT_TRIGGERED = const(0x0001)
+CONFIG_MODE_BVOLT_TRIGGERED = const(0x0002)
+CONFIG_MODE_SANDBVOLT_TRIGGERED = const(0x0003)
+CONFIG_MODE_ADCOFF = const(0x0004)
+CONFIG_MODE_SVOLT_CONTINUOUS = const(0x0005)
+CONFIG_MODE_BVOLT_CONTINUOUS = const(0x0006)
+CONFIG_MODE_SANDBVOLT_CONTINUOUS = const(0x0007)
 
 
 def _to_signed(num):
@@ -169,10 +174,10 @@ class INA226:
         # Now we can safely read the CURRENT register!
         raw_current = _to_signed(self._read_register(_REG_CURRENT))
         return raw_current * self._current_lsb
-    
+
     @property
     def power(self):
-        # INA226 stores the calculated power in this register        
+        # INA226 stores the calculated power in this register
         raw_power = _to_signed(self._read_register(_REG_POWER))
         # Calculated power is derived by multiplying raw power value with the power LSB
         return raw_power * self._power_lsb
@@ -205,21 +210,21 @@ class INA226:
            ..note :: These calculations assume a 0.1 shunt ohm resistor"""
         self._current_lsb = .0001
         self._cal_value = 512
-        self._power_lsb = .0025  
+        self._power_lsb = .0025
         self._write_register(_REG_CALIBRATION, self._cal_value)
-        
-        config = (_CONFIG_CONST_BITS |
-                  _CONFIG_AVGMODE_512SAMPLES |
-                  _CONFIG_VBUSCT_588us |
-                  _CONFIG_VSHUNTCT_588us |
-                  _CONFIG_MODE_SANDBVOLT_CONTINUOUS)
-        
+
+        config = (CONFIG_CONST_BITS |
+                  CONFIG_AVGMODE_512SAMPLES |
+                  CONFIG_VBUSCT_588us |
+                  CONFIG_VSHUNTCT_588us |
+                  CONFIG_MODE_SANDBVOLT_CONTINUOUS)
+
         self._write_register(_REG_CONFIG, config)
-    
+
     def set_calibration_custom(self, calValue=512, config=0x4127):
     # Set the configuration register externally by using the hex value for the config register
     # Value can be calculated with spreadsheet
     # Calibration value needs to be calculated seperately and passed as parameter too
-        self._cal_value = calValue        
+        self._cal_value = calValue
         self._write_register(_REG_CALIBRATION, self._cal_value)
         self._write_register(_REG_CONFIG, config)
